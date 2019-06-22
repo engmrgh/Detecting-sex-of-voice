10044543a83d0f81b326087a3654e07cd82b6fde
eng.mrgh <m.r.ghofrani@aut.ac.ir>
2019-06-22 15:18:32 +0430

    I think I have done the step 2 and I've drawn the power spectrum of the voices and saved them in images folder

--- Code.m
+++ Code.m
@@ -1,14 +1,21 @@
 % Step 2: Some matlab scripts to load voice files from files and drawing its power spectrum.
-% voices_path = "voices\";
-% voices = dir(voices_pat
-clc;
-clear all;
-MAX_VOICE_LENGTH = 2000000; % The maximum size of the voice file
 voice_folder = dir('voices\*.mp3'); % The folder in which the voice files has been saved
-voice = zeros(length(voice_folder), MAX_VOICE_LENGTH, 'double'); % An array of all the voices its demensions are 1 to 20000000
 for i = 1:length(voice_folder)
-    tmp = audioread(fullfile('voices', voice_folder(i).name)); % this line reads a audio file from the address determined by fullfile 
-    for j = 1:length(tmp)
-        voice(i,j) = tmp(j); % add manuly and one to one a index from voice file into where we want to save that
-    end
+    [voice,fs] = audioread(fullfile('voices', voice_folder(i).name));
+    voice_X = fft(voice);
+    hz = linspace(0,1000,fs/2+1);
+    psd = abs(voice_X).^2;
+    plot(hz,psd(1:length(hz)));
+    xlabel('hz'); ylabel('Amplitude');
+    title(strcat('Power spectrum of voice ',int2str(i)));
+    % This part is for saving the drawn figure
+    h = figure(1);
+    file_name = strcat('v',int2str(i),'.png');
+    saveas(h,fullfile('images',file_name));
 end
+
+%Step 3: 
+
+
+
+ 
